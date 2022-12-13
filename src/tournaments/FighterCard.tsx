37ad6dc61cfcb3b64630ca7473f57dfc52b38be8
Tim Kye <tim@kye.dev>
2022-12-12 17:04:33 -0800

    feat: add <FighterCard> text scaling

--- src/tournaments/FighterCard.tsx
+++ src/tournaments/FighterCard.tsx
@@ -1,18 +1,25 @@
+import { type JSX, mergeProps, createSignal, createEffect } from 'solid-js'
 import classnames from 'classnames'
-import { JSX, mergeProps } from 'solid-js'
 import { Fighter } from './types'
 
 import { noop } from '../util/functions'
+import { fighters } from './roster'
+
+function useFit(name: string): number {
+  return (1 / (name.length / 1.6)) * 55 * (name.includes(' ') ? 2 : 1)
+}
 
 export default function FighterCard(props: {
   class?: string
   fighter: Fighter
   onClick?: (f: Fighter) => void
 }): JSX.Element {
+  let name: HTMLElement | undefined
   const merged = mergeProps(
     { onClick: noop, lost: false, class: 'bg-slate-400' },
     props,
   )
+
   return (
     <div
       class={classnames(
@@ -21,7 +28,11 @@ export default function FighterCard(props: {
       )}
       onclick={[merged.onClick, props.fighter]}
     >
-      <span class="text-center text-ellipsis basis-4 grow-0 shrink-0">
+      <span
+        ref={name}
+        style={{ 'font-size': useFit(props.fighter.name) + 'px' }}
+        class={`text-center text-ellipsis basis-4 grow-0 shrink-0`}
+      >
         {props.fighter.name}
       </span>
       <img
